c9e1f47555a30e822a786899dad37720ec9f466d
reitraced <46302667+reitraced@users.noreply.github.com>
2019-05-12 18:13:04 -0400

    still doesnt work but idk i guess its closer to working

--- bot.py
+++ bot.py
@@ -9,11 +9,10 @@ token = f.read()
 f.seek(0)
 f.close()
 
-f = open('lastgame.txt', 'r')
+f = open('game.txt', 'r')
 lastgame = f.read()
 f.seek(0)
 f.close()
-
 
 @client.event
 async def on_ready():
@@ -23,5 +22,24 @@ async def on_ready():
 @client.command()
 async def ping(ctx):
     await ctx.send('Pong!')
+
+@client.command(pass_context=True, brief='Define Linux/UNIX terminology', description='Define terms, make sure to use commas to separate them. Ex. `shadowbot define linux, kernel`')
+async def define(ctx, arg):
+    if not arg:
+        await ctx.send('You need to give me terms to define')
+    else:
+        terms = {
+            "vim": "A common UNIX text editor. VI iMproved",
+            "emacs": "A common UNIX text editor with an extensive scripting language called Lisp. Many variants with the most popular being GNU Emacs",
+            "linux": "A family of computer operating systems using the Linux kernel.",
+            "kernel": "The core of an operating system. Handles all inputs and outputs",
+            "unix": "A standard for operating systems. Formerly one operating system that has since split. Here is a nice flow chart illustrating this split: https://upload.wikimedia.org/wikipedia/commons/7/77/Unix_history-simple.svg"
+            "vi": "A text editor used on most older UNIX systems. Mostly replaced by Vim",
+            "GNU": "The GNU Project is one of the largest FOSS projects in existence. It's software makes up the core of most Linux  operating systems"
+        }
+
+        answer = terms.get(arg)
+        await ctx.send("The definition of " + arg + "is " + answer )
+
 
 client.run(token)
