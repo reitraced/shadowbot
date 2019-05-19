813ffb5dbaea3d9b2a53552b9dae61c1b8840f1a
stophman1 <11683358+stophman1@users.noreply.github.com>
2019-05-19 13:33:17 -0500

    I made things better. Just like every time.

--- bot.py
+++ bot.py
@@ -14,25 +14,31 @@ lastgame = f.read()
 f.seek(0)
 f.close()
 
+
 @client.event
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
     await client.change_presence(activity=discord.Game(name=lastgame))
 
+
 @client.command(pass_context=True, brief="Print info about bot", description='Same as defining shadowbot')
 async def about(ctx):
     f = open('terms/shadowbot.txt', 'r')
-    about = f.read()
+    await ctx.send(f.read())
     f.seek(0)
     f.close()
-    await ctx.send(about)
+
 
 @client.command()
 async def ping(ctx):
     await ctx.send('Pong! {0}'.format(round(client.latency, 4)*1000) + ' ms')
 
-@client.command(pass_context=True, brief='Define Linux/UNIX terminology', description='Define terms. Ex. `shadowbot define linux` Please use lowercase I am offended by capital latters.')
-async def define(ctx, arg = None):
+
+@client.command(
+    pass_context=True,
+    brief='Define Linux/UNIX terminology',
+    description='Define terms. Ex. `shadowbot define linux`')
+async def define(ctx, arg=None):
     if arg:
         f = open('terms/' + arg.lower() + '.txt', 'r')
         answer = f.read()
@@ -46,12 +52,14 @@ async def define(ctx, arg = None):
     else:
         await ctx.send('You need to give me terms to define')
 
+
 @define.error
 async def define_error(ctx, error):
     await ctx.send('Could not recognize term')
 
+
 @client.command()
-async def make(ctx, arg1 = None, arg2 = None):
+async def make(ctx, arg1=None, arg2=None):
     if ctx.message.author.id == 177169904376610816 or 291663444883800064:
         if arg1 or arg2:
             f = open('terms/' + arg1 + '.txt', 'w+')
@@ -64,13 +72,11 @@ async def make(ctx, arg1 = None, arg2 = None):
         else:
             await ctx.send('You need to type a name and text for the file dumb.')
 
+
 @client.command()
 async def terms(ctx):
-            f = open('terms.txt', 'r')
-            read = f.read()
-            a = ctx.message.author
-            await ctx.send("Check your DMs for the terms.")
-            await a.send("```"+read+'\n```')
-            f.close()
+    await ctx.send("Check your DMs for the terms.")
+    await ctx.message.author.send("```"+open('terms.txt', 'r').read()+'\n```')
+    f.close()
 
 client.run(token)
